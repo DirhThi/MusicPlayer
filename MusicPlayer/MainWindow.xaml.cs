760494fd48f1d18477678523feefb62bbd37260f
DirhThi <20520771@gm.uit.edu.vn>
2023-01-02 00:35:05 +0700

    Home UI Update

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -17,13 +17,17 @@ using MusicPlayer.Properties;
 
 
 
+
 namespace MusicPlayer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+
+        private Button[] menuButton;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,41 +39,61 @@ namespace MusicPlayer
             border.Children.Add(uc);
         }
 
-
-
-        private void BtnChoose(Button btn)
-        {
-            
-        }
-
-
         private void Form_Load(object sender, RoutedEventArgs e)
         {
             UserControl uc = new Home();
             border.Children.Add(uc);
+            btnHome.Style = (Style)Application.Current.Resources["menuButtonChoose"];
         }
+
+        private void BtnChoose(Button bt)
+        {
+            menuButton = new Button[] { btnHome, btnSong, btnPlaylist, btnFavorite, btnExplore };
+            foreach( Button btn in menuButton )
+            {
+                if(btn==bt)
+                {
+                    Brush b = Brushes.Coral;
+                    btn.Background = b;
+                    btn.Style = (Style)Application.Current.Resources["menuButtonChoose"];
+                }
+                else
+                {
+                    Brush b = Brushes.Transparent;
+                    btn.Background = b;
+                    btn.Style = (Style)Application.Current.Resources["menuButton"];
+                }
+            }
+        }
+
+
+       
 
         private void Home_Click(object sender, RoutedEventArgs e)
         {
-            UserControl uc = new Home();
+            BtnChoose(btnHome);
+             UserControl uc = new Home();
             addUserControl(uc);
         }
 
 
         private void Song_Click(object sender, RoutedEventArgs e)
         {
+            BtnChoose(btnSong);
             UserControl uc = new ListSong();
             addUserControl(uc);
         }
 
         private void Playlist_Click(object sender, RoutedEventArgs e)
         {
+            BtnChoose(btnPlaylist);
             UserControl uc = new PlayList();
             addUserControl(uc);
         }
 
         private void Explore_Click(object sender, RoutedEventArgs e)
         {
+            BtnChoose(btnExplore);
             UserControl uc = new Explore();
             addUserControl(uc);
         }
